f879d5d125fc8f90d5cc8a8ce09bca562fea3e70
Adam Wester <awwester@gmail.com>
2019-12-21 14:59:52 -0500

    Install bootstrap and sass

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,14 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.selector-for-some-widget {
+  box-sizing: content-box;
 }
 </style>
