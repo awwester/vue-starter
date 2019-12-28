8ec9d95ca6b33b07fafed8afa5a91c12426f6b4b
Adam Wester <awwester@gmail.com>
2019-12-28 08:59:05 -0500

    Persist auth state

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 import authModule from './auth'
 
 Vue.use(Vuex)
 
+const vuexLocalStorage = new VuexPersistence({
+  storage: window.localStorage,
+  reducer: (state) => ({ auth: state.auth })
+})
+
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
   modules: {
     auth: authModule
-  }
+  },
+  plugins: [vuexLocalStorage.plugin]
 })
