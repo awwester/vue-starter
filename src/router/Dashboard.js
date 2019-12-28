370fe1c400a019b8c4946b5c2842dc6a1f104885
Adam Wester <awwester@gmail.com>
2019-12-28 08:45:11 -0500

    Protected dashboard routes

--- src/router/Dashboard.js
+++ src/router/Dashboard.js
@@ -1,3 +1,4 @@
+import store from '@/store'
 import DashboardBase from '@/views/dashboard/Base.vue'
 import Purchases from '@/views/dashboard/Purchases.vue'
 import Items from '@/views/dashboard/Items.vue'
@@ -8,5 +9,10 @@ export default {
   children: [
     { path: 'purchases', component: Purchases },
     { path: 'items', component: Items }
-  ]
+  ],
+  beforeEnter (to, from, next) {
+    const { token } = store.state.auth
+    if (token) next()
+    else next('/auth/login')
+  }
 }
